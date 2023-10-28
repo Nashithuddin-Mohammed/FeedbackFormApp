f2a8922c3b6c0ea4bbec7f54bc4692146ba55b66
Nashithuddin-Mohammed <nashfrk95@gmail.com>
2023-10-28 02:05:48 -0500

    added views

--- controller/server/index.js
+++ controller/server/index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const path = require('path')
 
+app.set('views', path.join(__dirname, '..', '..', 'views'))
 app.get('/', (req, res) =>
 {
-    res.send('Hello World!')
+    res.sendFile(path.join(__dirname, '..', '..', 'views', 'feedback.html'))
 })
 
 app.listen(port, () => 
