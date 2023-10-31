65ed4e80ec563fc4b0289f677203064ac2612ff4
Nashithuddin-Mohammed <nashfrk95@gmail.com>
2023-10-31 00:53:16 -0500

    Added views

--- controller/server/index.js
+++ controller/server/index.js
@@ -4,12 +4,19 @@ const port = 3000
 const path = require('path')
 
 app.set('views', path.join(__dirname, '..', '..', 'views'))
-app.get('/', (req, res) =>
-{
+app.use(express.static(path.join(__dirname, '..', '..', 'public')))
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '..', '..', 'views', 'feedback.html'))
 })
-
-app.listen(port, () => 
-{
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', '..', 'views', 'login.html'))
+})
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', '..', 'views', 'register.html'))
+})
+app.get('/feedback', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', '..', 'views', 'feedbackform.html'))
+})
+app.listen(port, () => {
     console.log('Server running at http://localhost:${port}')
 })
